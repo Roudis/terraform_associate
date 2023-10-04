5b9ffbec9a629cbcaab6f9af32fe1691489fede4
Charalampos Roudis <76263354+Roudis@users.noreply.github.com>
2023-10-04 21:52:29 +0300

    Create github.tf

---
+++ github.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    github = {
+        source = "integrations/github"
+        version = "~> 5.0"
+    }
+  }
+}
+
+#Configure the GitHub provider
+provider "github" {
+  token = "github_pat_xxxxxxxx"
+}
+
+resource "github_repository" "created_by_terraform" {
+  name = "example"
+  description = "My awesome codebase"
+}
